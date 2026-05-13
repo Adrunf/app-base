4995a5bb5d26bb1ae059dbe38a6147c27f82b9e8
KRUGER\arobayo <adrian.ordonez@zurich.com>
2026-05-13 13:13:45 -0500

    Creado proyecto MVC

---
+++ app-base-services/src/main/java/com/ec/app/microservices/PersonApplication.java
@@ -0,0 +1,24 @@
+package com.ec.app.microservices;
+
+import com.ec.app.microservices.config.BaseAppConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Application starter
+ *
+ * @author arobayo
+ */
+@Import({BaseAppConfiguration.class})
+@SpringBootApplication(scanBasePackages = {"com.ec.app"})
+@ComponentScan(basePackages = {"com.ec.app"})
+public class PersonApplication implements WebMvcConfigurer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonApplication.class);
+    }
+
+}
